3029aa1ffaf8869bc9ba203116665d899b892b3a
romerorocj-ga <charliejoelromero@gmail.com>
2023-10-25 16:21:05 -0400

    Init commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { getUser } from '../../../utilities/user-services'
+import AuthPage from '../Auth/AuthPage/AuthPage'
+import NavBar from '../NavBar/NavBar'
+import MainPage from '../MainPage/MainPage'
+
+function App() {
+  const [user, setUser] = useState(getUser())
+
+  return (
+    <main>
+      {user ?
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path='/' element={<MainPage />} />
+          </Routes>
+        </> :
+        <AuthPage setUser={setUser} />
+      }
+    </main>
+  )
+}
+
+export default App
